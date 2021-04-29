6e7b66e7c21511d1e2ceebb32633b96a7f8ead65
bleiderc <bleiderc@gmail.com>
2021-04-29 13:57:20 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timer';
+
+  ngOnInit() {
+    this.initInterval();
+}
+
+  initInterval() {
+    const countador = interval(10000);
+    countador.subscribe((seg: number) => {
+      seg *= 5;
+      console.log(seg);
+      this.presentAlert(seg);
+    });
+  }
+
+  presentAlert(seg: number) {
+    Swal.fire({
+      title: `Han pasado ${seg} segundos`,
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
 }
